d34cf5bd86da2f15e359a28110eba03e9a2f8b84
unknown <ssharma2@svsu.edu>
2018-06-19 20:36:09 -0400

    Google Oauth with passportjs

--- server.js
+++ server.js
@@ -1,18 +1,38 @@
 var express= require('express');
 var bodyParser=require('body-parser');
+const authRoutes=require('./routes/auth-routes');
+const resourceRoutes=require('./routes/resorce-routes')
 var _ = require('lodash');
 var app=express();
-var mongoose=require('./server/db/mongoose')
-var {Todo}=require('./server/models/todo');
-var {User}=require('./server/models/user');
-var {authenticate}=require('./server/middleware/authenticate')
+const keys=require('./config/keys');
+const passport=require('passport');
+const passportSetup=require('./config/passport-setup')
+var mongoose=require('./db/mongoose');
+const cookieSession=require('cookie-session')
+// var {Resource}=require('./models/resources');
+// var {User}=require('./models/user');
+// var {authenticate}=require('./server/middleware/authenticate')
 const port=process.env.PORT||3000;
+app.set('view engine','ejs')
+
+app.use(cookieSession({
+  maxAge:24*60*60*1000,
+  keys:[keys.session.cookieKey]
+}))
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+
 app.use(bodyParser.json())
+app.use('/auth',authRoutes)
+app.use('/resources',resourceRoutes)
 
 
 
-
-
+app.get('/login',(req,res)=>{
+  res.render('login.ejs')
+})
 
 
 
