5452c529a3fe9af43cf19c725bede6594999ffbd
unknown <ssharma2@svsu.edu>
2018-06-20 21:51:08 -0400

    congfig keys added for heroku

---
+++ config/keys.js
@@ -0,0 +1,9 @@
+module.exports={
+    google:{
+        clientID:"1017861134070-j3tgkfas46m4fildg4uod28jh7bln8p2.apps.googleusercontent.com",
+        clientSecret:'X-jkyZFGmqwlPARgi8bmbFf0'
+    },
+    session:{
+        cookieKey:'This is great!'
+    }
+}
